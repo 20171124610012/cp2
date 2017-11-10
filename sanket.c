f7746adf0568da416d89fa7e6cddba2b8b31a6eb
20171124610012 <33539174+20171124610012@users.noreply.github.com>
2017-11-10 12:08:35 +0530

    Add files via upload

---
+++ sanket.c
@@ -0,0 +1,18 @@
+	#include<stdio.h>
+   	
+  /*use of format specifier*/
+
+  void main()
+
+
+ {
+   printf("sanket\nsandesh");
+   printf("\nsanket\tsandesh");
+   printf("\nsanket\bsandesh");
+ }
+
+sanket
+sandesh
+sanket	sandesh
+sankesandeshdell@dell-OptiPlex-7040:~$ 
+
